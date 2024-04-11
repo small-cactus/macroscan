55cd0a16d9ae8d8918ba32e2aa7e60ef9d4027a0
Anthony Hayward <antmannacho@gmail.com>
2024-04-10 20:39:04 -0400

    fixed everything

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,12 +4,16 @@ import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native'; // Import useNavigation hook
+import Anthropic from "@anthropic-ai/sdk";
+import * as ImageManipulator from 'expo-image-manipulator';
 
 export default function MacroScanHome() {
   const [hasPermission, setHasPermission] = useState(null);
   const [image, setImage] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
   const navigation = useNavigation(); // Initialize navigation
+  const [extractedText, setExtractedText] = useState("");
+
 
   useEffect(() => {
     (async () => {
@@ -18,50 +22,54 @@ export default function MacroScanHome() {
     })();
   }, []);
 
+  const resizeImage = async (uri) => {
+    const result = await ImageManipulator.manipulateAsync(
+      uri,
+      [{ resize: { width: 800 } }], // Resize operation
+      { compress: 0.7, format: ImageManipulator.SaveFormat.JPEG, base64: true }
+    );
+    return result.base64; // Return the base64 string directly
+  };
+  
   const pickImage = async () => {
-    console.log("permission granted to pick image")
+    console.log("permission granted to pick image");
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
-      quality: 1,
-      base64: true,
+      quality: 1, // Consider setting a lower quality here if the images are still too large
     });
-
-    if (!result.canceled) {
-      console.log("photo from library submitted")
-      setImage(result.assets[0].uri);
+  
+    if (!result.canceled && result.assets) {
+      console.log("photo from library submitted");
+      const base64Data = await resizeImage(result.assets[0].uri);
+      setImage(result.assets[0].uri); // Display the original image in your UI
       setModalVisible(true);
-      if (result.assets[0].base64) {
-        sendImageToApi(result.assets[0].base64);
-        closeModal
-      }
+      sendImageToApi(base64Data); // Now sending the correct base64 string
     }
   };
-
+  
   const takePhoto = async () => {
     if (!hasPermission) {
-      console.log("permission granted to take photo")
+      console.log("permission granted to take photo");
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     }
-
+  
     if (hasPermission) {
-      console.log("photo taken")
+      console.log("photo taken");
       let result = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
         aspect: [4, 3],
-        quality: 1,
-        base64: true,
+        quality: 1, // Consider setting a lower quality here as well
       });
-
-      if (!result.canceled) {
-        setImage(result.assets[0].uri);
+  
+      if (!result.canceled && result.assets) {
+        console.log("photo from library submitted");
+        const base64Data = await resizeImage(result.assets[0].uri);
+        setImage(result.assets[0].uri); // Display the original image in your UI
         setModalVisible(true);
-        if (result.assets[0].base64) {
-          sendImageToApi(result.assets[0].base64);
-          closeModal
-        }
+        sendImageToApi(base64Data); // Now sending the correct base64 string
       }
     }
   };
@@ -71,45 +79,43 @@ export default function MacroScanHome() {
     console.log("modal closing")
   };
 
-  const sendImageToApi = async (base64Image) => {
-    const OPENAI_API_KEY = "OPENAI_API_KEY_REMOVED"; // Replace with your OpenAI API Key
-    const prompt = "What's in this image? Be very concise, 5 words."; // Your prompt text
-    console.log("attempting to send to api")
+
+  async function sendImageToApi(base64Image) {
     try {
-      const payload = {
-        model: "gpt-4-turbo",
-        messages: [
-          {
-            "role": "user",
-            "content": [
-              {"type": "text", "text": prompt},
-              {
-                "type": "image_url",
-                "image_url": {
-                  "url": `data:image/jpeg;base64,${base64Image}`
-                }
-              }
-            ],
-          }
-        ],
-        max_tokens: 150
-      };
-  
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', payload, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${OPENAI_API_KEY}`,
-        }
+      const anthropic = new Anthropic({
+        apiKey: "ANTHROPIC_API_KEY_REMOVED", // Ensure this is securely managed
       });
   
-      console.log(response.data.choices[0].message.content);
-      closeModal()
-      
-      // Handle the response here. For example, you might want to store it in state, display it, or navigate.
+      const msg = await anthropic.messages.create({
+        model: "claude-3-haiku-20240307",
+        max_tokens: 1000,
+        temperature: 0,
+        system: "You will be provided an image, If the image contains food, identify each food item and list all of its macronutrients in a structured format. Extract and include any relevant details from the image to support your analysis. For example, if you see a plate of spaghetti, categorize it as 'Carbohydrates' and estimate its macronutrient content. If the image does not feature food, simply respond with 'The image does not contain food. Try again.' Remember to always provide detailed estimates for any food items, avoiding conversational responses.",
+        messages: [{
+          "role": "user",
+          "content": [{
+            "type": "text",
+            "text": "If the image shows food, write down the macronutrient data for each item, being extremely specific with the amounts. Use any relevant details from the image to make your analysis as precise as possible. For instance, if the image is of a chicken salad, list it with categories such as 'Protein' for chicken, including specific estimated amounts. If the image does not show food, simply respond with 'The image does not contain food. Try again.' Do not add any other comments."
+          }, {
+            "type": "image",
+            "source": {
+              "type": "base64",
+              "media_type": "image/jpeg",
+              "data": base64Image
+            }
+          }]
+        }]
+      });
+  
+      const textResponse = msg.content[0].text;
+      console.log(textResponse); // Log the text content to the console
+      setExtractedText(textResponse); // Update the state to display in the UI
+      closeModal(); // Close the modal after processing the response
     } catch (error) {
-      console.error('Error sending image to API:', error);
+      console.error("Error sending message to Anthropic API:", error);
     }
-  };
+  }
+  
 
   return (
     <View style={styles.container}>
@@ -122,6 +128,13 @@ export default function MacroScanHome() {
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Details')}>
         <Text style={styles.buttonText}>Go to Details</Text>
       </TouchableOpacity>
+
+        {/* Display the extracted text here */}
+  {extractedText ? (
+    <View style={styles.extractedTextView}>
+      <Text style={styles.extractedText}>{extractedText}</Text>
+    </View>
+  ) : null}
 
       <Modal
         animationType="slide"
@@ -146,37 +159,31 @@ export default function MacroScanHome() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  button: {
-    backgroundColor: '#007bff',
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 10,
-    elevation: 2,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalView: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
+// Defining constants for colors and spacing can help in maintaining consistency and ease of adjustments.
+const colors = {
+  white: '#FFFFFF',
+  primaryBlue: '#007BFF',
+  modalBackground: 'rgba(0, 0, 0, 0.5)',
+  lightGrey: '#F0F0F0',
+  darkGrey: '#AAAAAA',
+  black: '#000000',
+};
+
+const spacing = {
+  small: 10,
+  medium: 20,
+  large: 35,
+  xLarge: 300, // For image dimensions or larger elements
+};
+
+const borderRadius = {
+  small: 5,
+  default: 20,
+};
+
+const shadow = {
+  default: {
+    shadowColor: colors.black,
     shadowOffset: {
       width: 0,
       height: 2,
@@ -185,22 +192,66 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  closeButton: {
-    backgroundColor: '#aaaaaa',
-    borderRadius: 20,
-    padding: 10,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+  },
+  button: {
+    backgroundColor: colors.primaryBlue,
+    borderRadius: borderRadius.default,
+    padding: spacing.small,
+    marginBottom: spacing.small,
     elevation: 2,
-    marginTop: 15,
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.modalBackground,
+  },
+  modalView: {
+    backgroundColor: colors.white,
+    borderRadius: borderRadius.default,
+    padding: spacing.large,
+    alignItems: 'center',
+    ...shadow.default,
+  },
+  closeButton: {
+    backgroundColor: colors.darkGrey,
+    borderRadius: borderRadius.default,
+    padding: spacing.small,
+    elevation: 2,
+    marginTop: spacing.medium,
   },
   closeButtonText: {
-    color: 'white',
+    color: colors.white,
     fontWeight: 'bold',
     textAlign: 'center',
   },
   imagePreview: {
-    width: 300, // Adjust the width as necessary
-    height: 300, // Adjust the height as necessary
-    borderRadius: 10,
-    marginBottom: 15,
+    width: spacing.xLarge, // Adjust the width as necessary
+    height: spacing.xLarge, // Adjust the height as necessary
+    borderRadius: borderRadius.small,
+    marginBottom: spacing.medium,
+  },
+  extractedTextView: {
+    margin: spacing.medium,
+    padding: spacing.small,
+    backgroundColor: colors.lightGrey, // Light grey background
+    borderRadius: borderRadius.small,
+  },
+  extractedText: {
+    color: colors.black, // Black text color
+    textAlign: 'center',
   },
 });
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,86 +1,103 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
 
 export default function SignInScreen({ navigation }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const BoxComponent = () => {
-    return (
-      <View style={styles.boxStyle}></View>
-    );
-  };
 
   const handleLogin = () => {
     if (username === 'Macro' && password === 'Scan') {
-      // If the credentials are correct, navigate to the HomeScreen
       navigation.replace('Home');
     } else {
-      // If the credentials are incorrect, display an alert
       Alert.alert('Invalid Credentials', 'The username or password is incorrect.');
     }
   };
 
   return (
-    <ImageBackground source={require("\favicon.png")} style={styles.background}>
-    <View style={styles.container}>
-      <Text style={styles.title}>Sign In to MacroScan</Text>
-      <TextInput 
-        style={styles.input} 
-        placeholder="Username" 
-        value={username} 
-        onChangeText={setUsername} 
-        autoCapitalize="none" 
-      />
-      <TextInput 
-        style={styles.input} 
-        placeholder="Password" 
-        value={password} 
-        onChangeText={setPassword} 
-        secureTextEntry 
-        autoCapitalize="none" 
-      />
-      <Button title="Sign In" onPress={handleLogin} />
-      <BoxComponent style={styles.boxStyle}></BoxComponent>
-    </View>
-    </ImageBackground>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Text style={styles.title}>Sign In to MacroScan</Text>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            placeholderTextColor="#A9A9A9"
+            value={username}
+            onChangeText={setUsername}
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#A9A9A9"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            autoCapitalize="none"
+          />
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  scrollView: {
+    flexGrow: 1,
     justifyContent: 'center',
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 20,
-    position: 'relative',
-    marginBottom: 350
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    color: '#000',
     textAlign: 'center',
+    marginTop: 60, // Adjust as needed to position the title at the top
+    marginBottom: 20,
   },
   input: {
-    backgroundColor: 'white',
-    marginBottom: 10,
+    width: '80%', // Adjust based on preference
+    backgroundColor: '#FFFFFF',
+    marginBottom: 12,
     paddingHorizontal: 15,
     paddingVertical: 10,
-    borderRadius: 5,
+    borderRadius: 20, // Increased borderRadius for more pronounced rounded corners
     borderWidth: 1,
     borderColor: 'gray',
+    color: '#000', // Text color
   },
-  boxStyle: {
-    height: 210,
-    width: 370,
-    backgroundColor: '#5E5E5E',
-    borderWidth: 2,
-    borderColor: '#000000',
-    borderRadius: 10,
-    position: 'absolute',
-    top: 88,
-    left: 9,
-    opacity: .5,
-    zIndex: -1,
-
+  button: {
+    width: '80%', // Match the input fields width
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 20, // Maintain rounded corners for consistency
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
