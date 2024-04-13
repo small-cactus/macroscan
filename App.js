a3d09a9888482832ef660c8f5b93c611a480138a
Anthony Hayward <antmannacho@gmail.com>
2024-04-12 23:01:27 -0400

    commit bar

--- App.js
+++ App.js
@@ -2,37 +2,82 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import WelcomeScreen from './screens/WelcomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import SignInScreen from './screens/SignInScreen';
-import { StatusBar } from 'expo-status-bar';
-
-// Make sure these are the correct versions in your package.json
-// import 'react-native-gesture-handler'; // At the top of your file
-// import 'react-native-reanimated'; // At the top of your file
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Details') {
+            iconName = focused ? 'list' : 'list-outline';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{ }} />
       <Tab.Screen name="Details" component={DetailsScreen} />
     </Tab.Navigator>
   );
 }
 
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  headerTitleStyle: {
+    fontSize: 18,
+  },
+  tabBarStyle: {
+    backgroundColor: '#ffffff',
+    borderTopColor: '#eeeeee',
+    paddingBottom: 3,
+  },
+  tabBarLabelStyle: {
+    fontSize: 12,
+  },
+  tabBarIconStyle: {
+    fontSize: 20,
+  },
+});
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerStyle: styles.headerStyle,
+          headerTintColor: '#fff',
+          headerTitleStyle: styles.headerTitleStyle,
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: false }}/>
         <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
